35efe01e9ac5a07a0d232b3f29ce59f3c72922ac
zahid khan <zahidkhan717816@gmail.com>
2024-12-04 22:37:32 +0500

    completed integration for edit reports

--- src/Compnents/Dashboard/Index.jsx
+++ src/Compnents/Dashboard/Index.jsx
@@ -6,12 +6,12 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 const Index = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <div>
         <Outlet />
       </div>
-    </div>
+    </>
 
   );
 };
