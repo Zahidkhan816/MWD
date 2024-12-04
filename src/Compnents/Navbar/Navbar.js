2b155e2483efaa873fe2755ac2b9442628325d4c
zahid khan <zahidkhan717816@gmail.com>
2024-12-04 22:41:56 +0500

    completed integration for edit reports

--- src/Compnents/Navbar/Navbar.js
+++ src/Compnents/Navbar/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/')
+    }
+
     return (
         <div className="header">
             <div className="header__logo">
-                <h4>
-                <strong>Documents Reader (POC)</strong>
+                <h4 onClick={handleNavigate} className="header__logo" style={{cursor:"pointer"}}>
+                        Documents Reader (POC)
                 </h4>
             </div>
             <nav className="navbar">
