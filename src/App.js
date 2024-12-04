a4b7a675ab563bac5f32e52f41e04e5893da8521
zahid khan <zahidkhan717816@gmail.com>
2024-12-04 22:32:07 +0500

    completed integration for edit reports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainPage from './Compnents/Dashboard/Index';
 import FileUpload from './Compnents/Dashboard/Components/FilerUpload';
-import TableList from './Compnents/Dashboard/Components/TableList';
+import TableList from './Compnents/Dashboard/Components/tableList';
 import Transcripts from './Compnents/Dashboard/Components/Transcripts';
 function App() {
   return (
